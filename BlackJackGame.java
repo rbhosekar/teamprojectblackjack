99e9f37a7035a865d219bf3515a8d2a0f707a484
Andrew Vazzano <ajvazzano@gmail.com>
2024-05-04 22:02:11 -0500

    added multiple players

--- BlackJackGame.java
+++ BlackJackGame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
-import java.time.Year;
+
 
 public class BlackJackGame extends JPanel {
     private Player[] players;
@@ -18,43 +18,40 @@ public class BlackJackGame extends JPanel {
     private JButton doubleDownButton;
     private Deck deck;
     private int betAmount;
+    private int currentPlayerIndex = 0;
+    private boolean dealerFirstCardHidden = true;
 
     public BlackJackGame(int numberOfPlayers) {
-
-        // initialize the players array
         players = new Player[numberOfPlayers];
 
-
-        // create buttons
         hitButton = new JButton("Hit");
         hitButton.addActionListener(e -> {
-            if (players[0].getScore() < 21) {
-                players[0].hit(deck.dealCard());
+            if (players[currentPlayerIndex].getScore() < 21) {
+                players[currentPlayerIndex].hit(deck.dealCard());
+                doubleDownButton.setEnabled(false);
                 updateHandDisplay();
             }
 
-            if (players[0].getScore() > 21) {
+            if (players[currentPlayerIndex].getScore() > 21) {
                 JOptionPane.showMessageDialog(this, "Bust!", "Bust", JOptionPane.INFORMATION_MESSAGE);
                 updateHandDisplay();
-                dealerTurn();
+                nextPlayerTurn();
             }
         });
 
         standButton = new JButton("Stand");
         standButton.addActionListener(e -> {
-            dealerTurn();
+            nextPlayerTurn();
         });
 
         doubleDownButton = new JButton("Double Down");
         doubleDownButton.addActionListener(e -> {
             betAmount *= 2;
-            players[0].doubleDown(deck.dealCard());
+            players[currentPlayerIndex].doubleDown(deck.dealCard());
             updateHandDisplay();
-            dealerTurn();
+            nextPlayerTurn();
         });
 
-
-        // create text areas for player's and dealer's hands
         playerHandArea = new JTextArea(10, 15);
         dealerHandArea = new JTextArea(10, 15);
         playerHandArea.setForeground(Color.YELLOW);
@@ -65,7 +62,6 @@ public class BlackJackGame extends JPanel {
         playerHandArea.setEditable(false);
         dealerHandArea.setEditable(false);
 
-        // create text areas for player's and dealer's hand values
         playerValueArea = new JTextArea(10, 5);
         dealerValueArea = new JTextArea(10, 5);
         playerValueArea.setForeground(Color.YELLOW);
@@ -76,15 +72,11 @@ public class BlackJackGame extends JPanel {
         playerValueArea.setEditable(false);
         dealerValueArea.setEditable(false);
 
-        // create text areas for player's balance
         playerBalanceArea = new JTextArea(1, 5);
         playerBalanceArea.setForeground(Color.YELLOW);
         playerBalanceArea.setFont(font2);
         playerBalanceArea.setEditable(false);
 
-
-
-        // create layout for the game GUI
         setLayout(new BorderLayout());
         JPanel playerPanel = new JPanel(new BorderLayout());
         playerPanel.add(playerHandArea, BorderLayout.CENTER);
@@ -94,15 +86,11 @@ public class BlackJackGame extends JPanel {
         dealerPanel.add(dealerHandArea, BorderLayout.CENTER);
         dealerPanel.add(dealerValueArea, BorderLayout.EAST);
 
-        // create bottom panel (buttons and player's balance)
         JPanel buttonPanel = new JPanel(new GridLayout(0, 1));
-
         buttonPanel.add(playerBalanceArea);
-
         buttonPanel.add(hitButton);
         buttonPanel.add(standButton);
         buttonPanel.add(doubleDownButton);
-
 
         add(playerPanel, BorderLayout.WEST);
         add(dealerPanel, BorderLayout.EAST);
@@ -114,10 +102,8 @@ public class BlackJackGame extends JPanel {
         dealerHandArea.setBackground(new Color(10, 50, 30));
         dealerValueArea.setBackground(new Color(10, 50, 30));
 
-        // deal initial cards to players and dealer
         dealInitialCards();
     }
-
 
     private void dealInitialCards() {
         betAmount = 100;
@@ -127,50 +113,50 @@ public class BlackJackGame extends JPanel {
         dealer.addCardToHand(deck.dealCard());
         dealer.addCardToHand(deck.dealCard());
 
-        // deal two cards to each player
         for (int i = 0; i < players.length; i++) {
             players[i] = new Player("player", 1000);
             players[i].addCardToHand(deck.dealCard());
             players[i].addCardToHand(deck.dealCard());
         }
 
-        // update GUI to display initial hands
         updateHandDisplay();
+        currentPlayerIndex = 0;
     }
 
-    private void newRound() {
-        betAmount = 100;
-        deck = new Deck();
-        deck.shuffle();
-        dealer.clearHand();
-        dealer.addCardToHand(deck.dealCard());
-        dealer.addCardToHand(deck.dealCard());
-        for (int i = 0; i < players.length; i++) {
-            players[i].clearHand();
-            players[i].addCardToHand(deck.dealCard());
-            players[i].addCardToHand(deck.dealCard());
+    private void nextPlayerTurn() {
+        doubleDownButton.setEnabled(true);
+        if (currentPlayerIndex == players.length - 1) {
+            dealerTurn();
+        } else {
+            currentPlayerIndex++;
+            updateHandDisplay();
         }
-        updateHandDisplay();
     }
 
     private void updateHandDisplay() {
-        // update text for player and dealer
-        playerHandArea.setText("Player's Hand:\n" + players[0].getHand());
-        dealerHandArea.setText("Dealer's Hand:\n" + dealer.getHand());
-        playerValueArea.setText("Value: " + players[0].getScore());
+        playerHandArea.setText(String.format("Player %d's Hand:\n%s", currentPlayerIndex + 1, players[currentPlayerIndex].getHand()));
+        dealerHandArea.setText("Dealer's Hand:\n" + "[Hidden]\n" + dealer.getHand());
+        dealerValueArea.setText("Value: ?");
+        playerValueArea.setText("Value: " + players[currentPlayerIndex].getScore());
+        playerBalanceArea.setText(String.format("Player %d's Balance:\n%s", currentPlayerIndex + 1, players[currentPlayerIndex].getBalance()));
+    }
+
+    private void revealDealer() {
+        dealerHandArea.setText("Dealer's Hand:\n" + dealer.revealHand());
         dealerValueArea.setText("Value: " + dealer.getScore());
-        playerBalanceArea.setText("Player's Balance: " + players[0].getBalance());
     }
 
     private void dealerTurn() {
+        revealDealer();
         Timer timer = new Timer(500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (dealer.getScore() < 17) {
                     dealer.addCardToHand(deck.dealCard());
-                    updateHandDisplay();
+                    revealDealer();
                 } else {
                     ((Timer) e.getSource()).stop();
+                    dealerFirstCardHidden = false; // Reveal dealer's first card
                     determineWinner();
                 }
             }
@@ -179,27 +165,44 @@ public class BlackJackGame extends JPanel {
     }
 
     private void determineWinner() {
-        int playerScore = players[0].getScore();
-        int dealerScore = dealer.getScore();
+        for (int i = 0; i < players.length; i++) {
+            Player player = players[i];
+            int playerScore = player.getScore();
+            int dealerScore = dealer.getScore();
+            String playerName = "Player " + (i + 1);
 
-        if (playerScore > 21) {
-            JOptionPane.showMessageDialog(this, "Dealer wins! Player busts.", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            players[0].collectWinnings(-betAmount);
-        } else if (dealerScore > 21) {
-            JOptionPane.showMessageDialog(this, "Player wins! Dealer busts.", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            players[0].collectWinnings(betAmount);
-        } else if (playerScore > dealerScore) {
-            JOptionPane.showMessageDialog(this, "Player wins!", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            players[0].collectWinnings(betAmount);
-        } else if (playerScore < dealerScore) {
-            JOptionPane.showMessageDialog(this, "Dealer wins!", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            players[0].collectWinnings(-betAmount);
-        } else {
-            JOptionPane.showMessageDialog(this, "Draw!", "Winner", JOptionPane.INFORMATION_MESSAGE);
+            if (playerScore > 21) {
+                JOptionPane.showMessageDialog(this, "Dealer wins! " + playerName + " busts.", "Results: " + playerName, JOptionPane.INFORMATION_MESSAGE);
+                player.collectWinnings(-betAmount);
+            } else if (dealerScore > 21) {
+                JOptionPane.showMessageDialog(this, playerName + " wins! Dealer busts.", "Results: " + playerName, JOptionPane.INFORMATION_MESSAGE);
+                player.collectWinnings(betAmount);
+            } else if (playerScore > dealerScore) {
+                JOptionPane.showMessageDialog(this, playerName + " wins!", "Results: " + playerName, JOptionPane.INFORMATION_MESSAGE);
+                player.collectWinnings(betAmount);
+            } else if (playerScore < dealerScore) {
+                JOptionPane.showMessageDialog(this, "Dealer wins! " + playerName, "Results: " + playerName, JOptionPane.INFORMATION_MESSAGE);
+                player.collectWinnings(-betAmount);
+            } else {
+                JOptionPane.showMessageDialog(this, "Draw!", "Results: " + playerName, JOptionPane.INFORMATION_MESSAGE);
+            }
         }
         newRound();
+        updateHandDisplay();
     }
 
-
+    private void newRound() {
+        dealerFirstCardHidden = true; // Hide dealer's first card again
+        betAmount = 100;
+        deck = new Deck();
+        deck.shuffle();
+        dealer.clearHand();
+        dealer.addCardToHand(deck.dealCard());
+        dealer.addCardToHand(deck.dealCard());
+        for (Player player : players) {
+            player.clearHand();
+            player.addCardToHand(deck.dealCard());
+            player.addCardToHand(deck.dealCard());
+        }
+    }
 }
-
--- Dealer.java
+++ Dealer.java
@@ -1,18 +1,32 @@
+import java.util.List;
+
 public class Dealer {
     private Hand hand;
-    
+
     public Dealer() {
         this.hand = new Hand();
     }
-    
+
     public void addCardToHand(Card card) {
         hand.addCard(card);
     }
-    
+
     public Hand getHand() {
+        // this method will hide the dealer's first card
+        Hand handWithoutFirst = new Hand();
+        List<Card> allCards = hand.getCards();
+
+        // add all the cards except the first one to the new hand object
+        for (int i = 1; i < allCards.size(); i++) {
+            handWithoutFirst.addCard(allCards.get(i));
+        }
+        return handWithoutFirst;
+    }
+
+    public Hand revealHand() {
         return hand;
     }
-    
+
     public void clearHand() {
         hand.getCards().clear();
     }
@@ -20,13 +34,13 @@ public class Dealer {
     public int getScore() {
         return hand.calculateScore();
     }
-    
+
     public void displayPartialHand() {
         System.out.println("Dealer's hand:");
         System.out.println("First card: [Hidden]");
         System.out.println("Second card: " + hand.getCards().get(1));
     }
-    
+
     public void displayFullHand() {
         System.out.println("Dealer's hand:");
         System.out.println(hand);
